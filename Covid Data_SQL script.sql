dbbbacc8888bc270c838ad276f7bb7d728a7b94b
Justified001 <126611928+Justified001@users.noreply.github.com>
2024-06-25 03:42:50 +0100

    SQL Portfolio Project script.sql

--- Covid Data_SQL script.sql
+++ Covid Data_SQL script.sql
@@ -3,20 +3,22 @@ FROM [Project Portfolio02]..[covid death]
 
 SELECT *
 FROM [Project Portfolio02]..[covid vaccination]
+	
 
--- select important data for analysis
+	-- select important data for analysis
+	
 Select Location, date, new_cases, total_cases, total_deaths, population
 FROM [Project Portfolio02]..[covid death]
 ORDER BY 1, 2
 
--- Total deaths Versus Total Cases
+	-- Total deaths Versus Total Cases
 Select continent,location, date,total_deaths, total_cases, population, cast(total_deaths as float)/cast(total_cases AS float)*100 AS DeathPercentage
 FROM [Project Portfolio02]..[covid death]
 WHERE location like '%states%' and continent is not null
 ORDER BY 1, 2
 
 
--- Explain the population by the total caeses (population VS Total cases)
+	-- Explain the population by the total caeses (population VS Total cases)
 Select continent, population, total_cases, 
 		cast(total_cases AS float)/cast(Population AS float)*100 AS "Infected Population Percentage"
 FROM [Project Portfolio02]..[covid death]
@@ -24,7 +26,7 @@ FROM [Project Portfolio02]..[covid death]
 WHERE continent is not null
 ORDER BY 2
 
--- Countries with higest infection rate
+	-- Countries with higest infection rate
 Select continent, population, MAX(total_cases) AS "Highest Infected Count", 
 		MAX(cast(total_cases AS float)/cast(Population AS float))*100 AS "Infected Population Percentage"
 FROM [Project Portfolio02]..[covid death]
@@ -33,7 +35,7 @@ WHERE continent is not null
 GROUP BY population, continent
 ORDER BY 4 DESC
 
--- Death Count per country population Group by continent
+	-- Death Count per country population Group by continent
 Select continent, MAX(CAST(total_deaths AS float)) AS "Highest Death Count" 
 FROM [Project Portfolio02]..[covid death]
 -- WHERE location like '%states%'
@@ -41,7 +43,7 @@ WHERE continent is Not NUll
 GROUP BY continent
 ORDER BY "Highest Death Count" DESC
 
--- showing continent with the highest death count
+	-- showing continent with the highest death count
 Select continent, MAX(CAST(total_deaths AS float)) AS "Highest Death Count" 
 FROM [Project Portfolio02]..[covid death]
 -- WHERE location like '%states%'
@@ -49,8 +51,8 @@ WHERE continent is Not NUll
 GROUP BY continent
 ORDER BY "Highest Death Count" DESC
 
--- total cases in the world each day 
-
+	
+	-- Total cases in the world each day 
 Select date, SUM(CAST(new_cases AS float)) AS "Highest infected Count" , SUM(CAST(new_deaths AS float)) AS "Highest Death Count",
 			(SUM(CAST(new_cases AS float))/SUM(CAST(nullif(new_deaths, 0) AS float)))*100 AS "Death Rate Percetage"
 FROM [Project Portfolio02]..[covid death]
@@ -59,9 +61,9 @@ WHERE continent is Not NUll
 GROUP BY date
 ORDER BY "Highest infected Count" DESC
 
+	
 -- Global Metrics
-
- --Population that took the vaccination by continent
+ 	--Population that took the vaccination by continent
  SELECT dea.continent, dea.date, dea.location, dea.population, vac.new_vaccinations,
  SUM(CAST(vac.new_vaccinations AS Float)) OVER(Partition by dea.location order by dea.location, dea.date)
  FROM [Project Portfolio02]..[covid death] dea
@@ -71,8 +73,7 @@ and dea.date = vac.date
 where dea.continent is not null
 order by 2, 3
 
--- Using CTE
-
+	-- Using CTE
 WITH PopVsVac (continent, date, location, population, new_vaccinations, "Rolling people vaccinated")
 AS
 (
@@ -89,8 +90,7 @@ SELECT *, ("Rolling people vaccinated"/population)*100 AS "Rolling people vaccin
 From PopVsVAc
 
 
-
- -- Create temporary table to store data
+ 	-- Create temporary table to store data
 CREATE TABLE #PercentagePopulationVaccinated (
     continent VARCHAR(100),
     date DATE,
@@ -100,7 +100,7 @@ CREATE TABLE #PercentagePopulationVaccinated (
     [Rolling people vaccinated] FLOAT
 );
 
--- Insert data into temporary table
+	-- Insert data into temporary table
 INSERT INTO #PercentagePopulationVaccinated
 SELECT 
     dea.continent, 
@@ -113,9 +113,9 @@ FROM
     [Project Portfolio02]..[covid death] dea
 JOIN 
     [Project Portfolio02]..[covid vaccination] vac ON dea.location = vac.location AND dea.date = vac.date
---WHERE  dea.continent IS NOT NULL;
 
--- Calculate percentage of population vaccinated
+
+	-- Calculate percentage of population vaccinated
 SELECT 
     *,
     ([Rolling people vaccinated] / population) * 100 AS [Rolling people vaccinated percentage]
@@ -124,7 +124,7 @@ FROM
 
 
 
--- Creating vew to store data for visualisation later
+	-- Creating vew to store data for visualisation later
  CREATE VIEW PercentagePopulationVaccinated AS
  SELECT 
     dea.continent, 
@@ -139,5 +139,7 @@ JOIN
     [Project Portfolio02]..[covid vaccination] vac ON dea.location = vac.location AND dea.date = vac.date
 WHERE  dea.continent IS NOT NULL;
 
+
+-- Data final check
 SELECT *
 FROM PercentagePopulationVaccinated
